d8aaa815b884ae0ddeb040829e8c42d38d51e409
geniushkg <geniushkg@gmail.com>
2016-10-06 11:41:57 +0530

    feat : check if existing user method

--- include/DbOperation.php
+++ include/DbOperation.php
@@ -20,8 +20,52 @@ class DbOperation
     }
 	
 	//todo: add methods to fetch debt hours for given userid
-	//todo: add method to add new userid and debt hours for new user_error
+
+	//todo: add method to add new userid and debt hours for new user
+	    // this will create a new user with default 10hours debt 
+	    public function createUserWithDebt($userId){
+ 
+        //First we will check whether the student is already registered or not
+        if (!$this->isUserExits($userId)) {
+           
+            //Crating an statement
+            $stmt = $this->con->prepare("INSERT INTO students(name, username, password, api_key) values(?, ?, ?, ?)");
+ 
+            //Binding the parameters
+            $stmt->bind_param("ssss", $name, $username, $password, $apikey);
+ 
+            //Executing the statment
+            $result = $stmt->execute();
+ 
+            //Closing the statment
+            $stmt->close();
+ 
+            //If statment executed successfully
+            if ($result) {
+                //Returning 0 means student created successfully
+                return 0;
+            } else {
+                //Returning 1 means failed to create student
+                return 1;
+            }
+        } else {
+            //returning 2 means user already exist in the database
+            return 2;
+        }
+    }
 	//todo: add method to modify debt hours for given user id
+	
+	
+	//todo: add method to check if user already exits
+	private function isUserExits($userId){
+		$stmt = $this->con->prepare("SELECT user_id from userdata WHERE user_id = ?");
+        $stmt->bind_param("i", $userID);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+	}
  
 }
 
