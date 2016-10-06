30c482e7875aa9802250c54647317c82b7b51534
geniushkg <geniushkg@gmail.com>
2016-10-06 11:26:13 +0530

    feat : constants and skeleton

---
+++ include/DbOperation.php
@@ -0,0 +1,28 @@
+<?php
+ 
+class DbOperation
+{
+    //Database connection link
+    private $con;
+ 
+    //Class constructor
+    function __construct()
+    {
+        //Getting the DbConnect.php file
+        require_once dirname(__FILE__) . '/DbConnect.php';
+ 
+        //Creating a DbConnect object to connect to the database
+        $db = new DbConnect();
+ 
+        //Initializing our connection link of this class
+        //by calling the method connect of DbConnect class
+        $this->con = $db->connect();
+    }
+	
+	//todo: add methods to fetch debt hours for given userid
+	//todo: add method to add new userid and debt hours for new user_error
+	//todo: add method to modify debt hours for given user id
+ 
+}
+
+?>
